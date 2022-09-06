b44e2977876d85565e954bee9c28186911b417e8
Thameembinyusuf <109033138+Thameembinyusuf@users.noreply.github.com>
2022-09-06 17:27:29 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -46,11 +46,11 @@ async def start(bot, update):
             [
                 [
                     InlineKeyboardButton(
-                        "Source code ⚡", url="https://github.com/Clinton-Abraham/UPLOADER-BOT"
+                        "Source code ⚡", url="https://github.com/thameembinyusuf/UPLOADER-BOT"
                     ),
-                    InlineKeyboardButton("Project Channel 👨🏻‍💻", url="https://t.me/Space_X_bots"),
+                    InlineKeyboardButton("Project Channel 👨🏻‍💻", url="https://t.me/tmybots"),
                 ],
-                [InlineKeyboardButton("Developer 👨‍⚖️", url="https://t.me/clinton_abraham")],
+                [InlineKeyboardButton("Developer 👨‍⚖️", url="https://t.me/thameem313")],
             ]
         ),
         reply_to_message_id=update.message_id
